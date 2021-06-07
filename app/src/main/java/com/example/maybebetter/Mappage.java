09652876a1a6572ab25199798cc1ce753387feca
MillanKelderman <17032@students.mrgs.school.nz>
2021-06-07 22:46:55 +1200

    Image Working and Zoom is working Need to Add ability for it too change where it can be zoomed in on Position wise.

--- app/src/main/java/com/example/maybebetter/Mappage.java
+++ app/src/main/java/com/example/maybebetter/Mappage.java
@@ -3,12 +3,40 @@ package com.example.maybebetter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.widget.ImageView;
 
 public class Mappage extends AppCompatActivity {
+
+    ImageView imageView;
+
+    ScaleGestureDetector scaleGestureDetector;
+    float scaleFactor = 1.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mappage2);
+
+        imageView = findViewById(R.id.imageView);
+
+        scaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+    }
+    //this detects the touch on the entire view
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return scaleGestureDetector.onTouchEvent(event);
+    }
+    //Sets up the scale factor and applies it to the image
+    private class ScaleListener extends  ScaleGestureDetector.SimpleOnScaleGestureListener {
+
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            scaleFactor *= scaleGestureDetector.getScaleFactor();
+            imageView.setScaleX(scaleFactor);
+            imageView.setScaleY(scaleFactor);
+            return true;
+        }
     }
 }
